cb136368297329d6a2818aeafd7667f13f2b2976
Chen Guo <guochen505@gmail.com>
2025-01-04 21:18:39 +0800

    import vercel speed insights

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,10 @@
 <script setup lang="ts">
+import SpeedInsights from '@vercel/speed-insights'
 </script>
 
 <template>
   <RouterView />
+  <SpeedInsights />
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
